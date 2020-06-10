63003badcd1099957cfd45df7420412c92558e6c
janaldoustorres <jat.torres@gmail.com>
2020-06-11 02:44:13 +0800

    ui improvements

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import bananabread1 from "./bananabread.jpg";
 import bananabread3 from "./bananabread3.jpg";
 import lemon from "./lemon.jpg";
 import Jumbotron from "react-bootstrap/Jumbotron";
-import instagram96 from "./icons8-instagram-96.png";
-import instagram48 from "./icons8-instagram-48.png";
+import instagramLogo from "./icons8-instagram-96.png";
+
+const defaultMessage = "Name:\nOrder:\nPayment:\nDelivery:";
+const uriDefaultMessage = encodeURIComponent(defaultMessage);
+const smsURI = `sms:09178001866;?&body=${uriDefaultMessage}`;
 
 function App() {
   return (
@@ -14,11 +17,11 @@ function App() {
       <Jumbotron>
         <div className="company-name">
           <img src={logo} alt="Logo" className="logo" />
-          <p className="lead ">Healthy. Delicious. Kind.</p>
           <a
             className="btn btn-primary btn-lg"
             href="#how-to-order"
             role="button"
+            id="btn-order"
           >
             Order
           </a>
@@ -37,53 +40,78 @@ function App() {
           </div>
         </div>
       </div>
-      <section>
-        <div className="section-title">The Bread</div>
-        <div className="instructions">Banana bread smothered in lemon zest</div>
+      <section id="the-bread">
+        <div className="row">
+          <div className="section-title center">The Bread</div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-sm flavor shadow">
+            <div>Walnut</div>
+          </div>
+          <div className="col-sm flavor shadow">
+            <div>Almond</div>
+          </div>
+          <div className="col-sm flavor shadow">
+            <div>Cinnamon</div>
+          </div>
+          <div className="col-sm flavor shadow">
+            <div>Original</div>
+          </div>
+        </div>
       </section>
       <section id="how-to-order">
         <div className="row">
-          <div className="col-sm-4">
-            <div className="section-title">How to Order</div>
-          </div>
-          <div className="col-sm-8">
-            <div className="instructions">
-              DM the following information on Instagram or text to 09178001866
+          <div className="section-title center">How to Order</div>
+        </div>
+        <div className="instructions">
+          <div className="instruction-step">
+            <div>
+              DM{" "}
+              <a href="https://www.instagram.com/breadforyouph/">
+                @breadforyouph
+              </a>{" "}
+              or Text to <a href={smsURI}>09178001866</a>
             </div>
+          </div>
+          <div className="instruction-step">
             <div className="message">
               <div className="message-line">
-                Name: <span>Juan de la Cruz</span>
+                Name: <span className="choice">Juan de la Cruz</span>
               </div>
               <div className="message-line">
-                Order: <span>2 banana bread</span>
+                Order: <span className="choice">2 original banana bread</span>
               </div>
               <div className="message-line">
-                Mode of payment: <span>COD or GCash to 09178001866</span>
+                Mode of payment: <span className="choice">COD</span>{" "}
+                <span className="or">or</span>{" "}
+                <span className="choice">GCash to 09178001866</span>
               </div>
               <div className="message-line">
-                Delivery option:{" "}
-                <span>Meet up location or address in Sta. Rosa, Laguna</span>
+                Delivery: <span className="choice">Meet up location</span>{" "}
+                <span className="or">or</span>{" "}
+                <span className="choice">Drop off address</span>
               </div>
             </div>
           </div>
         </div>
       </section>
       <section>
-        <div className="section-title">About us</div>
+        <div className="section-title center">About us</div>
         <div className="instructions">
           100% of profits go to COVID-19 Relief Fund
         </div>
       </section>
       <footer>
-        <div className="footer-item">
-          All profits will go towards COVID-19 charities
-        </div>
+        <div className="footer-item">breadforyouph</div>
         <div className="footer-item">
           <a href="https://www.instagram.com/breadforyouph/">
-          <picture>
-            <source srcSet={instagram96} media="(min-width: 550px)"/>
-            <img className="instagram-icon" srcSet={instagram48} alt="Instagram link"/>
-          </picture>
+            <picture>
+              <img
+                className="instagram-icon"
+                srcSet={instagramLogo}
+                alt="Instagram link"
+              />
+            </picture>
           </a>
         </div>
       </footer>
