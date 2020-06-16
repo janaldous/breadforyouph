f242e2ce45d692e6833e557083bd04d8de98523e
janaldoustorres <jat.torres@gmail.com>
2020-06-17 01:19:32 +0800

    added back functionality for order components

--- src/components/order/Order.test.tsx
+++ src/components/order/Order.test.tsx
@@ -8,12 +8,16 @@ describe("Order component", () => {
     expect(getByText("Your order")).toBeInTheDocument();
   });
 
-  it("shows delivery info page as 2nd page", () => {
+  it("shows delivery info page as 2nd page and goes back to 1st page", () => {
     const { getByText } = render(<Order />);
     
     fireEvent.click(getByText("Two more steps"));
 
     expect(getByText("Delivery information")).toBeInTheDocument();
+
+    fireEvent.click(getByText("< Back"))
+
+    expect(getByText("Your order")).toBeInTheDocument();
   });
 
   it("shows place order page as 3rd page", () => {
@@ -23,6 +27,10 @@ describe("Order component", () => {
     fireEvent.click(getByText("One more step"));
 
     expect(getByText("Order")).toBeInTheDocument();
+
+    fireEvent.click(getByText("< Back"))
+
+    expect(getByText("Delivery information")).toBeInTheDocument();
   });
 
   it("shows order confirmation as 4th page", () => {
@@ -33,5 +41,9 @@ describe("Order component", () => {
     fireEvent.click(getByText("Place order"));
 
     expect(getByText("Order confirmation")).toBeInTheDocument();
+
+    fireEvent.click(getByText("< Back"))
+
+    expect(getByText("Order")).toBeInTheDocument();
   });
 });
--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -13,12 +13,29 @@ export default function Order() {
     setStep((oldStep) => oldStep + 1);
   };
 
+  const handlePrev = () => {
+    setStep((oldStep) => oldStep - 1);
+  };
+
+  const getBackButton = (step: number) => {
+    switch (step) {
+      case 0:
+        return <a href={"/"}>{"< Back to Home"}</a>;
+      case 1:
+      case 2:
+      case 3:
+        return <div className="btn-back" onClick={handlePrev}>{"< Back"}</div>;
+      default:
+        throw new Error("invalid step");
+    }
+  };
+
   const getContent = (step: number) => {
     switch (step) {
       case 0:
         return <OrderInfo onNext={handleNext} />;
       case 1:
-        return <DeliveryInfo onNext={handleNext} />;
+        return <DeliveryInfo onNext={handleNext} onPrev={handlePrev} />;
       case 2:
         return <OrderSummary onNext={handleNext} />;
       case 3:
@@ -31,9 +48,7 @@ export default function Order() {
   return (
     <div className="order-container">
       <Navbar className="custom-navbar">
-        <div className="flex-1-only">
-          <a href={"/"}>{"< Back to Home"}</a>
-        </div>
+        <div className="flex-1-only">{getBackButton(step)}</div>
         <Navbar.Brand className="nav-brand" href="/">
           <img src={logo} alt="Logo" className="logo" />
         </Navbar.Brand>
