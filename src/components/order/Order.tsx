6906f49f6d4cbd0b080f8d188bac9c7dfe1dff62
janaldoustorres <jat.torres@gmail.com>
2020-06-17 00:14:44 +0800

    made Back to Home a link

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -4,6 +4,7 @@ import DeliveryInfo from "./DeliveryInfo";
 import OrderConfirmation from "./OrderConfirmation";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../logo.jpg";
+import { Link } from "react-router-dom";
 
 export default function Order() {
   const [step, setStep] = React.useState(0);
@@ -28,7 +29,7 @@ export default function Order() {
   return (
     <div className="order-container">
       <Navbar className="custom-navbar">
-        <div className="flex-1-only">{"< Back to Home"}</div>
+        <div className="flex-1-only"><Link to={"/"}>{"< Back to Home"}</Link></div>
         <Navbar.Brand className="nav-brand" href="/">
           <img src={logo} alt="Logo" className="logo" />
         </Navbar.Brand>
