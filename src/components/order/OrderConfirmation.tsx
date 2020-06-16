ffaad0b648f81c22acb6f2744da827ef9affe802
janaldoustorres <jat.torres@gmail.com>
2020-06-17 00:40:24 +0800

    fixed order confirmation title

--- src/components/order/OrderConfirmation.tsx
+++ src/components/order/OrderConfirmation.tsx
@@ -5,11 +5,12 @@ const OrderConfirmation: React.FC<OrderComponentProps> = (props) => {
   return (
     <div className="app-container">
       <section id="order">
-        <div className="row">
-          <div className="section-title center">Confirmed</div>
+        <div className="row justify-content-center">
+          <div className="description">
+            <div className="bold-title">Order confirmation</div>
+          </div>
         </div>
         <div className="row justify-content-center">
-          <div className="bold-title">Order confirmation</div>
           <div className="description">
             Success! Your order number is ... Expect the delivery at...
           </div>
