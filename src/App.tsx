d09313d76dcbd56fc34e55f4d662b7fd14d6d42c
janaldoustorres <jat.torres@gmail.com>
2020-06-22 23:49:18 +0800

    fixed css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./customer-components/home/Home";
-import Order from "./customer-components/order/Order";
-import instagramLogo from "./customer-components/home/icons8-instagram-96.png";
-import "./App.scss";
 import Admin from "./admin-components/Admin";
 import Customer from "./customer-components/Customer";
 
 function App() {
   return (
-    <div className="app-container">
       <Router>
         <Switch>
           <Route path="/admin">
@@ -20,21 +15,6 @@ function App() {
           </Route>
         </Switch>
       </Router>
-      <footer>
-        <div className="footer-item">breadforyouph</div>
-        <div className="footer-item">
-          <a href="https://www.instagram.com/breadforyouph/">
-            <picture>
-              <img
-                className="instagram-icon"
-                srcSet={instagramLogo}
-                alt="Instagram link"
-              />
-            </picture>
-          </a>
-        </div>
-      </footer>
-    </div>
   );
 }
 
--- src/customer-components/Customer.tsx
+++ src/customer-components/Customer.tsx
@@ -1,20 +1,42 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Home from "./home/Home";
 import Order from "./order/Order";
+import instagramLogo from "./icons8-instagram-96.png";
+import "./Customer.scss";
 
 const Customer: React.FC<{}> = () => {
   return (
-    <Router >
-      <Switch>
-        <Route path="/order">
-          <Order />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="app-container">
+      <Router>
+        <Switch>
+          <Route path="/order">
+            <Order />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      <footer>
+        <div className="footer-item">breadforyouph</div>
+        <div className="footer-item">
+          <a href="https://www.instagram.com/breadforyouph/">
+            <picture>
+              <img
+                className="instagram-icon"
+                srcSet={instagramLogo}
+                alt="Instagram link"
+              />
+            </picture>
+          </a>
+        </div>
+      </footer>
+    </div>
   );
 };
 
