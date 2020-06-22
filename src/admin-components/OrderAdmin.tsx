e299866ab1250815af63233f2d1b90ecf8c2c505
janaldoustorres <jat.torres@gmail.com>
2020-06-23 00:08:06 +0800

    show orders in table

--- src/admin-components/OrderAdmin.tsx
+++ src/admin-components/OrderAdmin.tsx
@@ -1,9 +1,42 @@
 import React from "react";
+import Table from "react-bootstrap/Table";
+import OrderApi from "../api/OrderApi";
+import { OrderDetail } from "breadforyou-fetch-api";
 
 const OrderAdmin: React.FC = () => {
+  const [orders, setOrders] = React.useState<Array<OrderDetail>>([]);
+
+  React.useEffect(() => {
+    OrderApi.getOrders().then((res) => {
+      setOrders(res);
+    });
+  }, []);
+
   return (
     <div className="admin-order">
       <h1>Admin order page</h1>
+      <Table>
+        <thead>
+          <tr>
+            <th>Order No</th>
+            <th>Customer</th>
+            <th>Order qty</th>
+            <th>Order status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((o) => (
+            <tr key={o.id}>
+              <td>{o.id}</td>
+              <td>
+                {`${o.user?.firstName} ${o.user?.lastName}`}
+              </td>
+              <td>view order</td>
+              <td>{o.tracking?.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
--- src/api/OrderApi.ts
+++ src/api/OrderApi.ts
@@ -1,8 +1,21 @@
-import { OrderDto, OrderControllerApi, OrderConfirmation } from "breadforyou-fetch-api";
+import {
+  OrderDto,
+  OrderControllerApi,
+  OrderConfirmation,
+  GetOrdersUsingGETStatusEnum,
+  OrderDetail,
+} from "breadforyou-fetch-api";
 
 export default class OrderApi {
   static postOrder(orderDto: OrderDto): Promise<OrderConfirmation> {
     const orderApi = new OrderControllerApi();
     return orderApi.orderUsingPOST({ orderDto });
+  }
+
+  static getOrders(
+    status?: GetOrdersUsingGETStatusEnum
+  ): Promise<Array<OrderDetail>> {
+    const orderApi = new OrderControllerApi();
+    return orderApi.getOrdersUsingGET({status});
   }
 }
