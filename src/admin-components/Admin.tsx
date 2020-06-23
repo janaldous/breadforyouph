0fec527adeacd73507e287e498e7dac009f5736d
janaldoustorres <jat.torres@gmail.com>
2020-06-23 22:51:22 +0800

    changed admin components to material-ui

--- src/admin-components/Admin.tsx
+++ src/admin-components/Admin.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import OrderAdmin from "./OrderAdmin";
+import "./Admin.scss";
 
 const Admin: React.FC<{}> = () => {
   return (
-    <Router basename="/admin">
-      <Switch>
-        <Route path="/orders">
-          <OrderAdmin />
-        </Route>
-        <Route path="/">
-          <Redirect to="/orders" />
-        </Route>
-      </Switch>
-    </Router>
+    <div className="admin-container">
+      <Router basename="/admin">
+        <Switch>
+          <Route path="/orders">
+            <OrderAdmin />
+          </Route>
+          <Route path="/">
+            <Redirect to="/orders" />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 };
 
--- src/admin-components/OrderAdmin.tsx
+++ src/admin-components/OrderAdmin.tsx
@@ -1,7 +1,13 @@
 import React from "react";
-import Table from "react-bootstrap/Table";
 import OrderApi from "../api/OrderApi";
 import { OrderDetail } from "breadforyou-fetch-api";
+import Table from "@material-ui/core/Table";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 const OrderAdmin: React.FC = () => {
   const [orders, setOrders] = React.useState<Array<OrderDetail>>([]);
@@ -14,29 +20,31 @@ const OrderAdmin: React.FC = () => {
 
   return (
     <div className="admin-order">
-      <h1>Admin order page</h1>
-      <Table>
-        <thead>
-          <tr>
-            <th>Order No</th>
-            <th>Customer</th>
-            <th>Order qty</th>
-            <th>Order status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {orders.map((o) => (
-            <tr key={o.id}>
-              <td>{o.id}</td>
-              <td>
-                {`${o.user?.firstName} ${o.user?.lastName}`}
-              </td>
-              <td>view order</td>
-              <td>{o.tracking?.status}</td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+      <h2>Admin order page</h2>
+      <TableContainer component={Paper}>
+        <Table aria-label="orders table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Order No</TableCell>
+              <TableCell>Customer</TableCell>
+              <TableCell>Order qty</TableCell>
+              <TableCell>Order status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell>{`${row.user?.firstName} ${row.user?.lastName}`}</TableCell>
+                <TableCell>view order</TableCell>
+                <TableCell>{row.tracking?.status}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
