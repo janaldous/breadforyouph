59c2931ad6eda925ec7c476356e99faa60432cc2
janaldoustorres <jat.torres@gmail.com>
2020-06-25 02:37:10 +0800

    added update status

--- src/admin-components/OrderDetail.tsx
+++ src/admin-components/OrderDetail.tsx
@@ -1,20 +1,46 @@
 import React from "react";
 import OrderApi from "../api/OrderApi";
-import { OrderDetail as OrderDetailModel } from "breadforyou-fetch-api";
+import {
+  OrderDetail as OrderDetailModel,
+  OrderTrackingStatusEnum,
+} from "breadforyou-fetch-api";
 import { useParams, Link } from "react-router-dom";
-import { Button } from "@material-ui/core";
 import Receipt from "./Receipt";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 
 const OrderDetail: React.FC = () => {
   const [order, setOrder] = React.useState<OrderDetailModel>();
+  const [open, setOpen] = React.useState(false);
+  const [status, setStatus] = React.useState<string>();
 
   const { id } = useParams();
 
   React.useEffect(() => {
     OrderApi.getOrder(id).then((res) => {
       setOrder(res);
+      setStatus(res.tracking?.status);
     });
   }, [id]);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleChange = (e: any) => {
+    setStatus(e.target.value);
+  };
 
   return (
     <div className="order-detail-container">
@@ -24,7 +50,12 @@ const OrderDetail: React.FC = () => {
       </div>
       <div>{id}</div>
       <div>Status</div>
-      <div>{order?.tracking?.status}</div>
+      <div>
+        {order?.tracking?.status}
+        <Button variant="contained" onClick={handleOpen}>
+          Update status
+        </Button>
+      </div>
       <div>Last updated</div>
       <div>{order?.tracking?.dateLastUpdated?.toDateString()}</div>
       <br />
@@ -59,6 +90,44 @@ const OrderDetail: React.FC = () => {
           Back
         </Button>
       </Link>
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        open={open}
+        onClose={handleClose}
+      >
+        <DialogTitle>Fill the form</DialogTitle>
+        <DialogContent>
+          <form>
+            <FormControl>
+              <InputLabel htmlFor="order-status">Status</InputLabel>
+              <Select
+                native={true}
+                value={status}
+                onChange={handleChange}
+                input={<Input id="order-status" />}
+              >
+                <option value={OrderTrackingStatusEnum.REGISTERED}>
+                  Registered
+                </option>
+                <option value={OrderTrackingStatusEnum.COOKING}>Cooking</option>
+                <option value={OrderTrackingStatusEnum.OTW}>OTW</option>
+                <option value={OrderTrackingStatusEnum.DELIVERED}>
+                  Delivered
+                </option>
+              </Select>
+            </FormControl>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
