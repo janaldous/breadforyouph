315a21af9e368a10ffa02af08a409152e2777dc7
janaldoustorres <jat.torres@gmail.com>
2020-07-18 13:27:47 +0800

    fixed api call

--- src/api/PublicApi.ts
+++ src/api/PublicApi.ts
@@ -1,10 +1,10 @@
 import { OrderDto, OrderConfirmation, DeliveryDate } from "./models";
 import axios, { AxiosPromise } from "axios";
-import {BASE_PATH} from "./runtime";
+import { BASE_PATH } from "./runtime";
 
 export default class OrderApi {
   static postOrder(orderDto: OrderDto): AxiosPromise<OrderConfirmation> {
-    return axios.post(`${BASE_PATH}/api/order`, { orderDto });
+    return axios.post(`${BASE_PATH}/api/order`, orderDto);
   }
 
   static getDeliveryDates(
